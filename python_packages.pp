01d217da1b15eec059c928b6396930f3e9495f93
Brandon Blinderman <brandon@BlindySupport.local>
2015-06-04 13:38:12 -0400

    divvied up into classes

--- python_packages.pp
+++ python_packages.pp
@@ -1,39 +1,40 @@
-package { "python-pip" :
-    ensure => "latest",
-}
+class python_packages {
+	package { "python-pip" :
+	ensure => "latest",
+	}
 
-package { "livestreamer" :
-    ensure => "latest",
-    provider => pip,
-}
+	package { "livestreamer" :
+    	ensure => "latest",
+    	provider => pip,
+	}
 
-package { "paramiko" :
-    ensure => "latest",
-    provider => pip,
-}
+	package { "paramiko" :
+    	ensure => "latest",
+    	provider => pip,
+	}
 
-package { "thefuck" :
-    ensure => "latest",
-    provider => pip,
-}
+	package { "thefuck" :
+    	ensure => "latest",
+    	provider => pip,
+	}
 
-package { "tldr" :
-    ensure => "latest",
-    provider => pip,
-}
+	package { "tldr" :
+    	ensure => "latest",
+    	provider => pip,
+	}
 
-package { "youtube-dl" :
-    ensure => "latest",
-    provider => pip,
-}
+	package { "youtube-dl" :
+    	ensure => "latest",
+    	provider => pip,
+	}
 
-package { "zenmap" :
-    ensure => "latest",
-    provider => pip,
-}
+	package { "zenmap" :
+    	ensure => "latest",
+    	provider => pip,
+	}		
 
-package { "mps-youtube" :
-    ensure => "latest",
-    provider => pip,
+	package { "mps-youtube" :
+    	ensure => "latest",
+    	provider => pip,
+	}
 }
-
